18068f039d56fca5e8cbe69890401e60f23be761
student <student@192.168.1.211>
2021-02-19 20:56:24 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day01/BMIDemo3.java
@@ -0,0 +1,9 @@
+package com.ocp.day01;
+
+public class BMIDemo3 {
+    public static void main(String[] args) {
+        BMIDemo2.printBMI("joker", 170, 90);
+        BMIDemo2.printBMI("Mary", 150, 50);
+        BMIDemo2.printBMI("Helen", 190, 70);
+    }
+}
