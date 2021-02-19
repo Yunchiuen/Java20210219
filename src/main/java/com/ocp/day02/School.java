bee298781381db2e6d5772c1f4b2affa6c847df3
student <student@192.168.1.211>
2021-02-19 21:18:47 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day02/School.java
@@ -0,0 +1,13 @@
+package com.ocp.day02;
+
+public class School {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        
+        s1.print();
+        s2.print();
+    }
+
+}
---
+++ src/main/java/com/ocp/day02/Student.java
@@ -0,0 +1,11 @@
+package com.ocp.day02;
+
+public class Student {
+    public String SchoolName;
+    public String name;
+    public int age;
+    
+    public void print(){
+        System.out.printf("%s %s %d\n",SchoolName,name,age);
+    }
+}
