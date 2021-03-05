a7c178ac049a4e161e6e1885750f8d36307811dc
student <student@192.168.1.211>
2021-03-05 20:11:21 +0800

    Java OCP

--- src/main/java/com/ocp/day06/StringCompare.java
+++ src/main/java/com/ocp/day06/StringCompare.java
@@ -1,20 +1,29 @@
 package com.ocp.day06;
 
 public class StringCompare {
-
+    
     public static void main(String[] args) {
         String s1 = new String("java");
         String s2 = new String("java");
         String s3 = "java";
-        String s4 = "java";
-
+        String s4 = "java";//POOL
+        String s5 = "Java";
+        
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
+        System.out.println("--String POOL--");
         System.out.println(s3 == s4);
         System.out.println(s3.equals(s4));
-        System.out.println(s1==s3);
+        System.out.println("-----");
+        System.out.println(s1 == s3);
         System.out.println(s1.equals(s3));
-        System.out.println(s1.intern() ==s3);
+        System.out.println("--把s1丟進字串詞裡面--");
+        System.out.println(s1.intern() == s3);//把s1丟進字串詞裡面
+        System.out.println("--忽略大小寫--");
+        System.out.println(s2.equalsIgnoreCase(s5));//忽略大小寫
+        System.out.println(s4.toUpperCase() == s5.toUpperCase());
+        System.out.println(s4.toLowerCase().equals(s5.toLowerCase()));
+        
         
     }
 }
---
+++ src/main/java/com/ocp/day06/StringUpdate.java
@@ -0,0 +1,22 @@
+package com.ocp.day06;
+
+public class StringUpdate {
+
+    public static void UpdateString(String x) {
+        x = x.concat("8");
+    }
+
+    public static void UpdateStringBuilder(StringBuilder y) {
+        y = y.append("8");
+    }
+
+    public static void main(String[] args) {
+
+        String x = "java";
+        StringBuilder y = new StringBuilder("java");
+        UpdateString(x);
+        UpdateStringBuilder(y);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
