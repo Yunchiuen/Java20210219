5bb2c10843c652fb09de95cb0bcda21df00a0366
student <student@192.168.1.211>
2021-03-05 19:08:27 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day06/StringCompare.java
@@ -0,0 +1,20 @@
+package com.ocp.day06;
+
+public class StringCompare {
+
+    public static void main(String[] args) {
+        String s1 = new String("java");
+        String s2 = new String("java");
+        String s3 = "java";
+        String s4 = "java";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.intern() ==s3);
+        
+    }
+}
