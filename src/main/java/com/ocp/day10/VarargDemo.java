d3ecd22b11f6ee35f3b02b392d0bb6d68e34b3a0
student <student@192.168.1.211>
2021-03-15 21:44:58 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day10/VarargDemo.java
@@ -0,0 +1,26 @@
+package com.ocp.day10;
+
+import java.util.stream.IntStream;
+
+public class VarargDemo {
+
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("helloworld");
+        }
+        
+        System.out.println("helloworld");
+        
+        System.out.println(add(10, 20));
+        System.out.println(add(10, 20, 30));
+        int[] values = {10, 20, 30, 40, 50};
+        System.out.println(add(values));
+        System.out.println(add(10));
+        System.out.println(add(10, 20, 30, 40));
+        System.out.println(add());
+    }
+
+    public static int add(int... values) {
+        return IntStream.of(values).sum();
+    }
+}
