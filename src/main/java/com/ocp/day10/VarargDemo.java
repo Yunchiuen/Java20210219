8a113b269a06d43ce873ac855a486e8f29f8b89a
student <student@192.168.1.211>
2021-03-15 21:46:06 +0800

    Java OCP

--- src/main/java/com/ocp/day10/VarargDemo.java
+++ src/main/java/com/ocp/day10/VarargDemo.java
@@ -6,7 +6,7 @@ public class VarargDemo {
 
     public static void main(String[] args) {
         if(true){
-            System.out.println("helloworld");
+            System.out.println("helloworld 1");
         }
         
         System.out.println("helloworld");
